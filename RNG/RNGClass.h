a6ac7a33857ed0cb7fd50a65ffc5d7caad564c5d
Ethan Brown <ethan.w.brown@gmail.com>
2014-09-22 21:39:19 +0200

    got rid of sprng

--- RNG/RNGClass.h
+++ RNG/RNGClass.h
@@ -1,96 +1,85 @@
-#ifndef RNGClass_H
-#define RNGClass_H
+#ifndef RNG_H
+#define RNG_H
 
 #include "../config.h"
-
-#define MULTIFILE_PROJECT
-
-#include "randomc.h"                   // define classes for random number generators
-#include "sfmt.h"                   // define classes for random number generators
-#include "stocc.h"                   // define classes for random number generators
-
-#ifndef MULTIFILE_PROJECT
-// If compiled as a single file then include these cpp files, 
-// If compiled as a project then compile and link in these cpp files.
-   // Include code for the chosen random number generator:
-   #include "sfmt.cpp"
-   // define system specific user interface:
-   #include "userintf.cpp"
-#endif
+#include <random>
 
 class RNG
 {
 private:
-  CRandomSFMT1 RanGen;  // make instance of random number generator
-  StochasticLib1 sto;  // make instance of random library
+  std::mt19937 rng;  // make instance of random number generator
 protected:
 public:
   // Constructor
   RNG(int seed)
-    : RanGen(seed), sto(seed)
+   : rng(seed), u_dist(0.,1.)
   {
     std::cout << "RNG seed: " << seed << "\n";
   }
 
+  std::uniform_real_distribution<RealType> u_dist;
+  std::uniform_int_distribution<IntType> uint_dist;
+  std::normal_distribution<RealType> normal_dist;
+
   // Random Functions
-  double unifRand();
-  double unifRand( const double a , const double b );
-  long unifRand( const long n );
-  void unifRand( Tvector& r );
-  void unifRand( Tvector& r , const double l );
-  double normRand( const double m , const double s );
-  void normRand( Tvector& r , const double m , const double s );
+  RealType unifRand();
+  RealType unifRand(const RealType a, const RealType b);
+  long unifRand(const long n);
+  void unifRand(Tvector& r);
+  void unifRand(Tvector& r, const RealType l);
+  RealType normRand(const RealType m, const RealType s);
+  void normRand(Tvector& r, const RealType m, const RealType s);
 };
 
 // Generate a random number between 0 and 1
 // return a uniform number in [0,1].
-inline double RNG::unifRand()
+inline RealType RNG::unifRand()
 {
-  return RanGen.Random();
+  return u_dist(rng);
 }
 
 // Generate a random number in a real interval.
 // param a one end point of the interval
 // param b the other end of the interval
 // return a inform rand numberin [a,b].
-inline double RNG::unifRand( const double a , const double b )
+inline RealType RNG::unifRand(const RealType a, const RealType b)
 {
-  return (b-a)*RanGen.Random() + a;
+  return (b-a)*unifRand() + a;
 }
 
 // Generate a random integer between 1 and a given value.
-// param n the largest value 
-// return a uniform random value in [1,...,n]
+// param n the largest value return a uniform random value in [1,...,n]
 inline long RNG::unifRand (const long n)
 {
-  return RanGen.IRandom(1,n);
+  return floor(unifRand()*n+1.);
 }
 
 // Generate a uniform random vector of length 1
-inline void RNG::unifRand (Tvector& r)
+inline void RNG::unifRand(Tvector& r)
 {
-  for (unsigned int iD = 0; iD < r.n_elem; iD += 1) 
+  for (unsigned int iD=0; iD<r.n_elem; iD++)
     r(iD) = unifRand(-1,1);
-  r /= norm(r, 2);
+  r /= norm(r,2);
 }
 
 // Generate a uniform random vector of length l
-inline void RNG::unifRand( Tvector& r , const double l )
+inline void RNG::unifRand(Tvector& r, const RealType l)
 {
   unifRand(r);
   r *= l;
 }
 
 // Generate a normal distribution random number
-inline double RNG::normRand( const double m , const double s )
-{				        /* mean m, standard deviation s */
-	return sto.Normal(m,s);
+inline RealType RNG::normRand(const RealType m, const RealType s)
+{
+  /* mean m, standard deviation s */
+  return normal_dist(rng)*s + m;
 }
 
 // Generate a normal random vector of length 1
-inline void RNG::normRand( Tvector& r , const double m , const double s )
+inline void RNG::normRand(Tvector& r, const RealType m, const RealType s)
 {
-  for (unsigned int iD = 0; iD < r.n_elem; iD += 1) 
+  for (unsigned int iD=0; iD<r.n_elem; iD++)
     r(iD) = normRand(m,s);
 }
 
