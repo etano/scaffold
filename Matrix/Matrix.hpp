d62e46de19f7f1a6bffcd3963b0e8bd71ed42005
Ethan Brown <ethan.w.brown@gmail.com>
2014-11-26 17:55:46 +0100

    generalized eigen include

--- Matrix/Matrix.hpp
+++ Matrix/Matrix.hpp
@@ -2,7 +2,7 @@
 #define SCAFFOLD_MATRIX
 
 #ifdef USE_EIGEN
-  #define EIGEN_MATRIXBASE_PLUGIN "/Users/ethan/src/simpimc/src/Utils/Matrix/eigen_matrix_additions.h"
+  #define EIGEN_MATRIXBASE_PLUGIN SCAFFOLD_DIR "/Matrix/eigen_matrix_additions.h"
   #include "eigen.hpp"
 #else
   #include "armadillo.hpp"
