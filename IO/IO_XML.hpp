2a718a0e0a18eff65a17b6557814a23445d4a72f
Ethan Brown <ethan.w.brown@gmail.com>
2015-02-10 16:51:29 +0100

    Merged in IO_XML

--- IO/IO_XML.hpp
+++ IO/IO_XML.hpp
@@ -20,15 +20,26 @@ inline float convertConstChar(const char * val) { return atof(val); }
 template <>
 inline int convertConstChar(const char * val) { return atoi(val); }
 
+struct Node
+{
+  std::string name;
+  std::vector<Node> childNodes;
+  std::map<std::string,std::string> attributes;
+};
+
 struct Input
 {
+  // Original buffer
   std::vector<char> buffer;
-  rapidxml::xml_node<> * node;
 
+  // My node
+  Node node;
+
+  // Constructors
   Input() {};
-  Input(std::vector<char> t_buffer, rapidxml::xml_node<> *t_node)
-    : buffer(t_buffer), node(t_node)
-  {}
+  Input(Node &t_node, std::vector<char> &t_buffer)
+    : node(t_node), buffer(t_buffer)
+  {};
 
   // Loads settings structure from the specified XML file
   void load(const std::string &filename)
@@ -39,10 +50,43 @@ struct Input
     buffer.push_back('\0');
     rapidxml::xml_document<> doc;
     doc.parse<0>(&buffer[0]);
-    node = doc.first_node("Input");
+    rapidxml::xml_node<> *rapidxml_node = doc.first_node("Input");
+    loadXML(rapidxml_node, node);
   }
 
-  inline std::string getName() { return std::string(node->name()); }
+  void loadXML(rapidxml::xml_node<> *rapidxml_node, Node &my_node)
+  {
+    // Load name
+    my_node.name = std::string(rapidxml_node->name());
+
+    // Load attributes
+    for (rapidxml::xml_attribute<> *attr = rapidxml_node->first_attribute(); attr; attr = attr->next_attribute())
+      my_node.attributes[std::string(attr->name())] = std::string(attr->value());
+
+    // Load child nodes
+    for (rapidxml::xml_node<> *rapidxml_child=rapidxml_node->first_node(); rapidxml_child; rapidxml_child=rapidxml_child->next_sibling()) {
+      Node childNode;
+      loadXML(rapidxml_child, childNode);
+      my_node.childNodes.push_back(childNode);
+    }
+
+  }
+
+  void printXML(Node &my_node)
+  {
+    // Print name
+    std::cout << "name: " << my_node.name << std::endl;
+
+    // Print attributes
+    for(auto attr: my_node.attributes)
+      std::cout << " " << attr.first << " : " << attr.second << std::endl;
+
+    // Print children
+    for(auto& child: my_node.childNodes)
+      printXML(child);
+  }
+
+  inline std::string getName() { return node.name; }
 
   inline std::string getString()
   {
@@ -53,49 +97,49 @@ struct Input
   template <class T>
   inline T getAttribute(std::string name)
   {
-    rapidxml::xml_attribute<char> *att(node->first_attribute(name.c_str()));
-    if (att == 0) {
+    if (node.attributes.find(name) == node.attributes.end()) {
       std::cerr << "ERROR: Attribute " << name << " not found in node " << getName() << "!" << std::endl;
       abort();
     } else
-      return convertConstChar<T>(att->value());
+      return convertConstChar<T>(node.attributes[name].c_str());
   }
 
   template <class T>
   inline T getAttribute(std::string name, T deflt)
   {
-    rapidxml::xml_attribute<char> *att(node->first_attribute(name.c_str()));
-    if (att == 0)
+    if (node.attributes.find(name) == node.attributes.end())
       return deflt;
     else
-      return convertConstChar<T>(att->value());
+      return convertConstChar<T>(node.attributes[name].c_str());
   }
 
   inline Input getChild(std::string name)
   {
-    rapidxml::xml_node<> *child = node->first_node(name.c_str());
-    if (child == 0) {
-      std::cerr << "ERROR: Child " << name << " not found in node " << getName() << "!" << std::endl;
-      abort();
-    } else {
-      Input in(buffer,child);
-      return in;
+    for(auto& child: node.childNodes) {
+      if (child.name == name) {
+        Input in(child,buffer);
+        return in;
+      }
     }
+    std::cerr << "ERROR: Child " << name << " not found in node " << getName() << "!" << std::endl;
+    abort();
   }
 
   inline std::vector<Input> getChildList(std::string name) {
     std::vector<Input> ins;
-    for (rapidxml::xml_node<> *child=node->first_node(name.c_str()); child; child=child->next_sibling()) {
-      Input in(buffer,child);
-      ins.push_back(in);
+    for(auto& child: node.childNodes) {
+      if (child.name == name) {
+        Input in(child,buffer);
+        ins.push_back(in);
+      }
     }
     return ins;
   }
 
   inline std::vector<Input> getChildList() {
     std::vector<Input> ins;
-    for (rapidxml::xml_node<> *child=node->first_node(); child; child=child->next_sibling()) {
-      Input in(buffer,child);
+    for(auto& child: node.childNodes) {
+      Input in(child,buffer);
       ins.push_back(in);
     }
     return ins;
